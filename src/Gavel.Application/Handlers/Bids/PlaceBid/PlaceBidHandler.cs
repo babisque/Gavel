31488d54519320f4ff1be6564f83db2121199ee5
babisque <rodrigobabisque1@gmail.com>
2025-11-20 00:43:02 -0300

    Validate auction item status and end time in PlaceBidHandler before processing bids

--- src/Gavel.Application/Handlers/Bids/PlaceBid/PlaceBidHandler.cs
+++ src/Gavel.Application/Handlers/Bids/PlaceBid/PlaceBidHandler.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Gavel.Domain.Entities;
+using Gavel.Domain.Enums;
 using Gavel.Domain.Exceptions;
 using Gavel.Domain.Interfaces;
 using Gavel.Domain.Interfaces.Services;
@@ -19,16 +20,25 @@ public class PlaceBidHandler(
     {
         var auctionItem = await unitOfWork.AuctionItems.GetByIdAsync(request.AuctionItemId);
         
-        var bid = mapper.Map<Bid>(request);
-        bid.TimeStamp = DateTime.UtcNow;
-        var createdBid = await unitOfWork.Bids.CreateAsync(bid);
+        if (auctionItem is null)
+            throw new NotFoundException($"Auction item {request.AuctionItemId} not found");
+        
+        if (auctionItem.Status != AuctionStatus.Active)
+            throw new ConflictException("Auction is not active.");
+    
+        if (auctionItem.EndTime < DateTime.UtcNow)
+            throw new ConflictException("Auction has ended.");
         
         if (request.Amount <= auctionItem.CurrentPrice)
             throw new ConflictException("Bid must be higher than current price.");
-
+        
+        var bid = mapper.Map<Bid>(request);
+        bid.TimeStamp = DateTime.UtcNow;
         auctionItem.CurrentPrice = request.Amount;
+        
+        await unitOfWork.Bids.CreateAsync(bid);
         await unitOfWork.AuctionItems.UpdateAsync(auctionItem);
-
+        
         try
         {
             await unitOfWork.CompleteAsync(cancellationToken);
@@ -38,6 +48,6 @@ public class PlaceBidHandler(
             throw new ConflictException("The price has changed since you loaded the page. Please refresh and try again.");
         }
             
-        await bidNotificationService.NotifyNewBidAsync(createdBid);
+        await bidNotificationService.NotifyNewBidAsync(bid);
     }
 }
